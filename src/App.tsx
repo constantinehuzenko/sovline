1eef58d54a20b4c47480b33f09912ed9cd888b47
costa <kossguzenko@gmail.com>
2022-01-25 22:04:11 +0100

    github pages config

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          HUI app for interviews
+          HUI app for interviews Costa and Vlad Production
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
