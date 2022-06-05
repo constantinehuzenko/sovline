b1f2f5bec61387d8feb608238f1aad1798d8ede1
Kostiantyn.Huzenko <Kostiantyn.Huzenko@shell.com>
2022-06-05 14:17:18 +0200

    check error handling

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,47 @@ import { HashRouter } from "react-router-dom";
 
 const store = setupStore();
 
+class ErrorBoundary extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  // componentDidCatch(error, errorInfo) {
+  // You can also log the error to an error reporting service
+  // logErrorToMyService(error, errorInfo);
+  // }
+
+  render() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line react/destructuring-assignment
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    // eslint-disable-next-line react/destructuring-assignment
+    return this.props.children;
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>,
+  <ErrorBoundary>
+    <React.StrictMode>
+      <GlobalStyle />
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
+    </React.StrictMode>
+  </ErrorBoundary>,
   document.getElementById("root")
 );
 
