cea33d589b96b12b80ab4fa2f50e99ccb3d072fe
Constantine Huzenko <Kostiantyn.Huzenko@shell.com>
2022-08-21 18:43:27 +0200

    feat: added english questions (#14)

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { AddQuiz } from "./addQuiz/AddNewQuiz.page";
+// import { AddQuiz } from "./addQuiz/AddNewQuiz.page";
 import { QuizPage } from "./quiz/Quiz.page";
 
 export const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<QuizPage />} />
-      <Route path="1" element={<AddQuiz />} />
+      {/* <Route path="/" element={<AddQuiz />} /> */}
     </Routes>
   );
 };
